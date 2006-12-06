c36bff1a22b476681eb789d6807f832b3d60ee4f
Mauricio Fernandez <mfp@acm.org>
2006-12-06 17:36:57 +0000

    lib/future/storage.rb: implemented the BasicStore (trivial).

---
+++ lib/future/storage.rb
@@ -0,0 +1,88 @@
+
+require 'future/paths'
+require 'digest/sha1'
+require 'fileutils'
+
+module Future
+
+class BasicStore
+  class FileSelector
+    attr_reader :path, :sha1digest
+    DEFAULT_OPTIONS = {
+      :sha1digest => nil,
+      :path       => nil,
+    }
+    def initialize(options = {})
+      options = DEFAULT_OPTIONS.merge(options)
+      unless options[:sha1digest] || options[:path]
+        raise ArgumentError, "Need either file path or sha1digest."
+      end
+      @path       = options[:path]
+      @sha1digest = options[:sha1digest]
+    end
+  end
+
+  DEFAULT_INIT_OPTIONS = {
+    :default_name => "data",
+  }
+  def initialize(base_path = Paths.cache_dir, options = {})
+    options = DEFAULT_INIT_OPTIONS.merge(options)
+    @base_path    = base_path
+    @default_name = options[:default_name]
+    FileUtils.mkdir_p(@base_path)
+  end
+
+  DEFAULT_STORE_OPTIONS = {
+    :sha1digest => nil,
+    :preserve_name => false,
+  }
+  def store(filename, data, options = {})
+    options = DEFAULT_STORE_OPTIONS.merge(options)
+    digest  = options[:sha1digest] || Digest::SHA1.hexdigest(data)
+    level1, level2, rest = split_sha1(digest)
+    relpath = File.join(level1, level2, rest)
+    path    = File.join(@base_path, relpath)
+    FileUtils.mkdir_p(path)
+    basename = options[:preserve_name] ? filename : @default_name
+    dest     = File.join(path, basename)
+    tmpname  = File.join(path, "#{Process.pid}-#{Thread.object_id}")
+    File.open(tmpname, "wb"){|f| f.write data}
+    File.rename(tmpname, dest)
+
+    FileSelector.new(:path => File.join(relpath, basename), 
+                     :sha1digest => digest)
+  end
+
+  def read(fileselector)
+    fh = open(fileselector)
+    fh.read
+  ensure
+    fh.close if fh
+  end
+
+  def open(fileselector)
+    if (sha1 = fileselector.sha1digest)
+      level1, level2, rest = split_sha1(sha1)
+      basename = File.basename(fileselector.path || @default_name)
+      File.open(File.join(@base_path, level1, level2, rest, basename), "rb")
+    else
+      File.open(File.join(@base_path, fileselector.path), "rb")
+    end
+  end
+
+  def include?(fileselector)
+    if (sha1 = fileselector.sha1digest)
+      level1, level2, rest = split_sha1(sha1)
+      File.directory?(File.join(@base_path, level1, level2, rest))
+    else
+      File.directory(File.join(@base_path, fileselector.path))
+    end
+  end
+
+  private
+  def split_sha1(hexdigest)
+    return /(..)(..)(.*)/.match(hexdigest).captures
+  end
+end
+
+end #  Future
