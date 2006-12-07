452ada06342adac35e3d05605a62b96fe30c5fea
Mauricio Fernandez <mfp@acm.org>
2006-12-07 15:22:29 +0000

    lib/future/recursive_downloader.rb: did HTML file rewriting to make refs absolute/point to downloaded resources.

--- lib/future/recursive_downloader.rb
+++ lib/future/recursive_downloader.rb
@@ -3,6 +3,7 @@ require 'future/paths'
 require 'future/utils'
 require 'open-uri'
 require 'hpricot'
+require 'stringio'
 
 module Future
 
@@ -11,6 +12,8 @@ class RecursiveDownloader
     @uri = uri
     @fetched = {}
     @redirected = {}
+    @html_process_pending = {}
+    @css_process_pending  = {}
   end
 
   def download(options = {})
@@ -21,26 +24,28 @@ class RecursiveDownloader
 
   def fetch_references_html(uri)
     raise "No data for #{uri.to_s}." unless io = @fetched[uri]
+    uri2 = URI.parse(uri)
     doc = Hpricot(io)
     %w[img script].each do |resource|
       doc.search(resource).each do |res|
         # FIXME: check type = "text/javascript", etc?
         if (src = res.attributes["src"])
-          resolved_uri = uri.merge(src).normalize
-          next if @fetched[resolved_uri] || @redirected[resolved_uri]
+          resolved_uri = uri2.merge(src).normalize
+          next if @fetched[resolved_uri.to_s] || @redirected[resolved_uri.to_s]
           next if resolved_uri.query.to_s != ''
-          fetch(resolved_uri)
+          actual_uri, = fetch(resolved_uri)
+          @redirected[resolved_uri.to_s] = actual_uri
         end
       end
     end
     %w{//link[@type='text/css']}.each do |query|
       doc.search(query).each do |res|
         if (src = res.attributes["href"])
-          resolved_uri = uri.merge(src).normalize
-          next if @fetched[resolved_uri] || @redirected[resolved_uri]
+          resolved_uri = uri2.merge(src).normalize
+          next if @fetched[resolved_uri.to_s] || @redirected[resolved_uri.to_s]
           next if resolved_uri.query.to_s != ''
           actual_uri, = fetch(resolved_uri)
-          @redirected[resolved_uri] = actual_uri
+          @redirected[resolved_uri.to_s] = actual_uri
           fetch_recursive(actual_uri)
         end
       end
@@ -55,19 +60,64 @@ class RecursiveDownloader
     @fetched.keys
   end
 
+  BASIC_LINK_RESOLVER = lambda do |src, dest| 
+    File.basename(dest.path)
+  end
+
+  def processed_file(uri, &resolver_block)
+    resolver_block ||= BASIC_LINK_RESOLVER
+    actual_uri = @redirected[uri.normalize.to_s].to_s
+    case
+    when @html_process_pending[actual_uri]
+      StringIO.new(rewrite_html(actual_uri, &resolver_block))
+    when @css_process_pending[actual_uri]
+      StringIO.new(rewrite_css(actual_uri, &resolver_block))
+    else
+      @fetched[actual_uri]
+    end
+  end
+
+
   private
+  REWRITE_ATTRIBUTE_DATA = {
+    "//img" => "src", "//script" => "src", 
+    "//a[@href]" => "href", "//link[@type='text/css']" => "href"
+  }
+  def rewrite_html(uri, &resolver_block)
+    @fetched[uri.to_s].rewind
+    doc = Hpricot(@fetched[uri.to_s].read)
+    REWRITE_ATTRIBUTE_DATA.each_pair do |resource, attr|
+      doc.search(resource).each do |res|
+        if (src = res.attributes[attr])
+          src = URI.parse(src)
+          resolved_uri = URI.parse(uri).merge(src).normalize
+          if (actual_uri = @redirected[resolved_uri.to_s])
+            new_rel_uri = resolver_block.call(uri, actual_uri)
+            res.attributes[attr] = new_rel_uri.to_s
+          else
+            res.attributes[attr] = resolved_uri.to_s
+          end
+        end
+      end
+    end
+    doc.to_s
+  end
+
   def fetch(uri, options = {})
     io = OpenURI.open_loop(uri, options)
-    @fetched[io.base_uri] = io
+    @fetched[io.base_uri.to_s] = io
+    @redirected[uri.to_s] = io.base_uri
     [io.base_uri, io]
   end
 
   def fetch_recursive(uri)
-    case @fetched[uri].content_type
+    case @fetched[uri.to_s].content_type
     when %{text/html}
-      fetch_references_html(uri)
+      @html_process_pending[uri.to_s] = true
+      fetch_references_html(uri.to_s)
     when %{text/css}
-      fetch_references_css(uri)
+      @css_process_pending[uri.to_s] = true
+      fetch_references_css(uri.to_s)
     end
   end
 
--- test/test_recursive_downloader.rb
+++ test/test_recursive_downloader.rb
@@ -9,8 +9,9 @@ require 'future/recursive_downloader.rb'
 class TestRecursiveDownloader < Test::Unit::TestCase
   include Future
   class DummyURI
+    attr_reader :path
     def initialize(path, content_type = "application/octet-stream")
-      @path = path
+      @path = File.expand_path(path)
       @content_type = content_type
     end
 
@@ -26,6 +27,10 @@ class TestRecursiveDownloader < Test::Unit::TestCase
       end
       buf
     end
+
+    def to_s
+      URI.parse("file://" + @path).to_s
+    end
   end
 
   class LoggedRecursiveDownloader < Future::RecursiveDownloader
@@ -40,26 +45,28 @@ class TestRecursiveDownloader < Test::Unit::TestCase
   end
 
   def test_download
-    uri = DummyURI.new(File.join(File.dirname(__FILE__), "data", "recursive-dler.html"))
+    uri = DummyURI.new(File.join(File.expand_path(File.dirname(__FILE__)), 
+                                 "data", "recursive-dler.html"))
     fetcher = RecursiveDownloader.new(uri)
     num_files = fetcher.download(:proxy => false)
     assert_equal(1, num_files)
   end
 
   def test_download_recursive
-    uri = DummyURI.new(File.join(File.dirname(__FILE__), "data", "recursive-dler.html"),
+    uri = DummyURI.new(File.join(File.expand_path(File.dirname(__FILE__)), 
+                                 "data", "recursive-dler.html"),
                        "text/html")
     fetcher = LoggedRecursiveDownloader.new(uri)
     num_files = fetcher.download(:proxy => false)
     assert_equal(1, num_files)
-    assert_equal({:fetch_references_html => [[uri]]}, fetcher.calls)
+    assert_equal({:fetch_references_html => [["file:#{uri.path}"]]}, fetcher.calls)
 
     uri = DummyURI.new(File.join(File.dirname(__FILE__), "data", "recursive-dler.html"),
                        "text/css")
     fetcher = LoggedRecursiveDownloader.new(uri)
     num_files = fetcher.download(:proxy => false)
     assert_equal(1, num_files)
-    assert_equal({:fetch_references_css => [[uri]]}, fetcher.calls)
+    assert_equal({:fetch_references_css => [["file:#{uri.path}"]]}, fetcher.calls)
   end
 
 
@@ -75,7 +82,8 @@ class TestRecursiveDownloader < Test::Unit::TestCase
 
     def fetch(uri, options = {})
       data, content_type = get_contents(uri)
-      io = @fetched[uri] = StringIO.new(data)
+      io = @fetched[uri.to_s] = StringIO.new(data)
+      @redirected[uri.to_s] = uri
       class << io; self end.module_eval do 
         define_method(:content_type) { content_type }
       end
@@ -83,7 +91,7 @@ class TestRecursiveDownloader < Test::Unit::TestCase
     end
   end
 
-  def test_recursive_download
+  def test_recursive_download_basic
     base = File.expand_path(File.dirname(__FILE__))
     uri = URI.parse("file://" + (File.join(base, "data", "recursive-dler.html")))
     fetcher = DummyRecursiveDownloader.new(uri)
@@ -99,6 +107,23 @@ http://eigenclass.org/update.png
 http://eigenclass.org/xml.png
 EOF
     assert_equal(files.split(/\n/), fetcher.downloaded_files.map{|x| x.to_s.gsub(/^file:#{base}/,"")}.sort)
+  end
+
+  def test_processed_file
+    base = File.expand_path(File.dirname(__FILE__))
+    base_uri = URI.parse("file://" + File.join(base, "data/"))
+    uri = base_uri + "recursive-downloader2.html"
+    fetcher = DummyRecursiveDownloader.new(uri)
+    fetcher.set_contents(uri, File.read(uri.path), "text/html")
+    fetcher.set_contents(base_uri + "css/test.css", "css/test.css", "text/css")
+    fetcher.set_contents(base_uri + "test.css", "test.css", "text/css")
+    fetcher.set_contents(base_uri + "pics/foo.gif", "pics/foo.gif", "image/gif")
+    fetcher.set_contents(base_uri + "foo/bar/baz.html", "foo/bar/baz.html", "text/html")
+    num_files = fetcher.download(:proxy => false)
+    assert_equal(4, num_files)
+    processed_file = File.join(File.expand_path(File.dirname(__FILE__)), "data", "recursive-downloader2-processed.html")
+    assert_equal(File.read(processed_file),
+                 fetcher.processed_file(uri).string)
   end
 
 end
