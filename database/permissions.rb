4941aa967ee7be11e9fa29b4198bfff9068b6b07
Mauricio Fernandez <mfp@acm.org>
2006-12-11 21:59:59 +0000

    database/permissions.rb: item_id instead of items_id, etc.

--- database/permissions.rb
+++ database/permissions.rb
@@ -1,9 +1,9 @@
 ['items', 'sets', 'landmarks', 'users'].each do |tbl|
   tables[tbl+'_groups'] = {
-    tbl+'_id' => [[tbl, :id], 'not null'],
+    tbl[0..-2]+'_id' => [[tbl, :id], 'not null'],
     :group_id => [['groups', :id], 'not null'],
     :can_modify => [:boolean, 'not null', 'default false']
   }
-  constraints << [tbl+'_groups', :unique, [tbl+'_id', :group_id]]
+  constraints << [tbl+'_groups', :unique, [tbl[0..-2]+'_id', :group_id]]
 end
 
