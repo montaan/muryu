db3d0642256e86771fad2ff158d95d2b179ee9ff
kig <kig@misfiring.net>
2006-12-13 03:48:30 +0000

    small milestone runner change

--- test/milestone_runner.rb
+++ test/milestone_runner.rb
@@ -2,7 +2,5 @@ milestone_name = ARGV[0]
 exit unless milestone_name
 milestone_log = File.open(milestone_name + ".log", "w")
 STDOUT.reopen milestone_log
-$stdout = STDOUT
 STDERR.reopen milestone_log
-$stderr = STDERR
 load milestone_name
