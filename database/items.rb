d7e63a2a425fae0f0a0c4b203aac7d777db21eca
Mauricio Fernandez <mfp@acm.org>
2006-12-13 16:12:49 +0000

    database/items.rb: added internal_path, source, referrer columns. Typo in indexes.

--- database/items.rb
+++ database/items.rb
@@ -1,16 +1,20 @@
 tables["items"] = {
   :continuous_index => [:serial, 'unique', 'not null'],
   :path => [:text, 'unique', 'not null'],
+  :internal_path => [:text, 'not null'],
   :size => [:bigint, 'not null', 'default 0'],
   :sha1_hash => ['character(40)'],
   :deleted => [:boolean, 'not null', 'default false'],
   
   :created_at => [:timestamp, 'not null'],
   :modified_at => [:timestamp, 'default now()'],
+
+  :source   => [:text],
+  :referrer => [:text],
   
   :owner_id => [['users',:id], 'not null'],
   :mimetype_id => [['mimetypes', :id], 'not null', 'default 15'],
   :metadata_id => [['metadata', :id], 'not null'],
 }
 
-indexes << ['items', 'filename']
+indexes << ['items', 'path']
