98b8d5e532465a182e4eff53c2deed1c8cae82d4
kig <kig@misfiring.net>
2006-12-13 18:25:22 +0000

    groups closer to completion

--- lib/future/permissions/access_control.rb
+++ lib/future/permissions/access_control.rb
@@ -26,26 +26,91 @@ module AccessControl
 end
 
 
+module AccessControlClass
+
+  def rfind_all(user, h={})
+    qs = parse_query(h)
+    qs = qs.sub("FROM", "FROM groups g, users_groups ug, #{table_name}_groups tg,").
+            sub("WHERE", "WHERE ug.user_id = #{user.id}
+            AND ug.group_id = g.id
+            AND tg.group_id = g.id
+            AND tg.#{table_name[0..-2]}_id = #{table_name}0.id
+            AND")
+    puts qs
+    q = DB::Conn.exec(qs)
+    idx = -1
+    q.map{|i| new q, idx+=1 }
+  rescue
+    p h
+    raise
+  end
+  
+  def rfind(user, h={})
+    rfind_all(user, h.merge(:limit => 1, :columns => :all)).first
+  end
+
+  def create(h)
+    h = h.clone
+    ugroup = h[:owner].group
+    if h.delete(:public)
+      pgroup = Groups.public
+    end
+    it = super(h)
+    DB::Tables.const_get(to_s+"Groups").create(
+      :group_id => ugroup, "#{table_name[0..-2]}_id" => it, :can_modify => true
+    )
+    DB::Tables.const_get(to_s+"Groups").create(
+      :group_id => pgroup, "#{table_name[0..-2]}_id" => it, :can_modify => false
+    ) if pgroup
+    it
+  end
+
+end
+
+
 class Items < DB::Tables::Items
 include AccessControl
+extend AccessControlClass
 
 end
 
 
 class Groups < DB::Tables::Groups
 include AccessControl
+extend AccessControlClass
+
+  def self.public
+    find_or_create("namespace" => "global", "name" => "public")
+  end
+
+  def self.rfind_all(user, h={})
+    qs = parse_query(h)
+    qs = qs.sub("FROM", "FROM groups g, users_groups ug,").
+            sub("WHERE", "WHERE ug.user_id = #{user.id}
+            AND ug.group_id = groups0.id
+            AND")
+    puts qs
+    q = DB::Conn.exec(qs)
+    idx = -1
+    q.map{|i| new q, idx+=1 }
+  rescue
+    p h
+    raise
+  end
 
 end
 
 
 class Sets < DB::Tables::Sets
 include AccessControl
+extend AccessControlClass
 
 end
 
 
 class Landmarks < DB::Tables::Landmarks
 include AccessControl
+extend AccessControlClass
 
 end
 
--- test/milestones/5_groups.rb
+++ test/milestones/5_groups.rb
@@ -23,30 +23,29 @@ include Future
   def teardown
     @user.logout
     @user2.logout
-    unregister
   end
 
   def test_group
-    assert(Groups.find_all(@user, :name => 'foo fighters').empty?)
-    assert(Groups.find_all(@user2, :name => 'foo fighters').empty?)
+    assert(Groups.rfind_all(@user, :name => 'foo fighters').empty?)
+    assert(Groups.rfind_all(@user2, :name => 'foo fighters').empty?)
 
     ff = Groups.create(:name => 'foo fighters', :owner => @user, :public => false)
-    assert(Groups.find_all(@user, :name => 'foo fighters').size == 1)
-    assert(Groups.find_all(@user2, :name => 'foo fighters').empty?)
+    assert(Groups.rfind_all(@user, :name => 'foo fighters').size == 1)
+    assert(Groups.rfind_all(@user2, :name => 'foo fighters').empty?)
     
     bb = Groups.create(:name => 'baz buzzers', :owner => @user2, :public => true)
-    assert(Groups.find_all(@user, :name => 'baz buzzers').size == 1)
-    assert(Groups.find_all(@user2, :name => 'baz buzzers').size == 1)
+    assert(Groups.rfind_all(@user, :name => 'baz buzzers').size == 1)
+    assert(Groups.rfind_all(@user2, :name => 'baz buzzers').size == 1)
 
     ff.add_member @user2
-    assert(Groups.find_all(@user2, :name => 'foo fighters').size == 1)
+    assert(Groups.rfind_all(@user2, :name => 'foo fighters').size == 1)
     
     ff.remove_member @user2
-    assert(Groups.find_all(@user2, :name => 'foo fighters').empty?)
+    assert(Groups.rfind_all(@user2, :name => 'foo fighters').empty?)
 
     Groups.delete(:name => 'baz buzzers')
-    assert(Groups.find_all(@user, :name => 'baz buzzers').size == 0)
-    assert(Groups.find_all(@user2, :name => 'baz buzzers').size == 0)
+    assert(Groups.rfind_all(@user, :name => 'baz buzzers').size == 0)
+    assert(Groups.rfind_all(@user2, :name => 'baz buzzers').size == 0)
   end
   
 end
