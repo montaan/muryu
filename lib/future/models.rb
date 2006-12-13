2148eff7b8b253fbe265990840ed98c8b7cbc956
Mauricio Fernandez <mfp@acm.org>
2006-12-13 19:23:42 +0000

    lib/future/models.rb: fixed.

--- lib/future/models.rb
+++ lib/future/models.rb
@@ -3,7 +3,7 @@ require 'uri'
 Dir[File.join(File.dirname(__FILE__), "**", "*.rb")].each do |file|
   file = File.expand_path(file)
   dest = URI.parse("file://" + file)
-  base = URI.parse(File.expand_path(File.dirname(__FILE__)))
+  base = URI.parse("file://" + File.expand_path(File.dirname(__FILE__)) + "/")
   require("future/" + base.route_to(dest).to_s)
 end
 
