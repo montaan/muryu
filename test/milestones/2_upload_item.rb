3411d3a868509005d487f8805ef2f48900c73103
Mauricio Fernandez <mfp@acm.org>
2006-12-13 23:29:19 +0000

    test/milestones/2_upload_item.rb: upload the items repeatedly to make sure that filename collisions are handled OK.

--- test/milestones/2_upload_item.rb
+++ test/milestones/2_upload_item.rb
@@ -8,23 +8,27 @@ include Future
 
   def test_create_text_post
     user = Users.login('foo', 'bar', 'my_session')
-    item = {:user => user, :text => "Hello, World!"}
-    i = Uploader.upload item
-    assert(Items.find(:id => i))
-    assert_equal(i.read, "Hello, World!")
+    5.times do 
+      item = {:user => user, :text => "Hello, World!"}
+      i = Uploader.upload item
+      assert(Items.find(:id => i))
+      assert_equal(i.read, "Hello, World!")
+    end
   end
 
   def test_create_image_post
     user = Users.login('foo', 'bar', 'my_session')
-    i = Uploader.upload(:user => user, :io => File.open("#{DATADIR}/guide-2.jpg"),
-                        :referrer => "http://poignantguide.net/ruby/chapter-2.html",
-                        :source   => "http://poignantguide.net/ruby/i/chapter.poignant.guide-2.jpg",
-                        :filename => "chapter.poignant.guide-2.jpg")
-    assert(Items.find(:id => i))
-    assert_equal(i.read.size, File.size("#{DATADIR}/guide-2.jpg"))
-    metadata = Metadata.find(:id => i.metadata_id)
-    assert_equal(191, metadata.width)
-    assert_equal(174, metadata.height)
+    5.times do 
+      i = Uploader.upload(:user => user, :io => File.open("#{DATADIR}/guide-2.jpg"),
+                          :referrer => "http://poignantguide.net/ruby/chapter-2.html",
+                          :source   => "http://poignantguide.net/ruby/i/chapter.poignant.guide-2.jpg",
+                          :filename => "chapter.poignant.guide-2.jpg")
+      assert(Items.find(:id => i))
+      assert_equal(i.read.size, File.size("#{DATADIR}/guide-2.jpg"))
+      metadata = Metadata.find(:id => i.metadata_id)
+      assert_equal(191, metadata.width)
+      assert_equal(174, metadata.height)
+    end
   end
 
 end
