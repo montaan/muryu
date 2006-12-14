2a26623a0fa1808d2f8311f63f107e1d6d9eb380
Mauricio Fernandez <mfp@acm.org>
2006-12-14 16:57:00 +0000

    config.rb: set database for production, development envs.

--- config.rb
+++ config.rb
@@ -1,21 +1,21 @@
 
-database_configuration = 
+database_conf = 
 {
   "test" => {
     :host => nil, :port => nil, :options => nil, :database => "future_test",
     :login => nil, :password => nil
   },
   "development" => {
-    :host => nil, :port => nil, :options => nil, :database => nil,
+    :host => nil, :port => nil, :options => nil, :database => "future_development",
     :login => nil, :password => nil
   },
   "production" => {
-    :host => nil, :port => nil, :options => nil, :database => nil,
+    :host => nil, :port => nil, :options => nil, :database => "future_production",
     :login => nil, :password => nil
   },
 }
 
 Future.configuration do |c|
-  c.database_configuration = database_configuration[c.environment]
+  c.database_configuration = database_conf[c.environment]
   # c.logger = Logger.new(c.log_dir + "#{c.environment}.log")
 end
