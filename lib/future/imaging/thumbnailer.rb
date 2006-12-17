a7d4fb73c4c6daa03716f96adc44ce42991d1be8
kig <kig@misfiring.net>
2006-12-17 16:26:09 +0000

    thumbnailer size specification

--- lib/future/imaging/thumbnailer.rb
+++ lib/future/imaging/thumbnailer.rb
@@ -4,8 +4,8 @@ require 'pathname'
 
 class Pathname
 
-  def thumbnail(thumb_filename)
-    mimetype.thumbnail(self, thumb_filename)
+  def thumbnail(thumb_filename, thumb_size=128)
+    mimetype.thumbnail(self, thumb_filename, thumb_size)
   end
 
   def mimetype
@@ -18,38 +18,38 @@ end
 module Mimetype
 
   # Default thumbnailer uses the mimetype icon as the thumbnail.
-  def thumbnail(filename, thumb_filename)
+  def thumbnail(filename, thumb_filename, thumb_size=128)
     if to_s =~ /video/
-      video_thumbnail(filename, thumb_filename)
+      video_thumbnail(filename, thumb_filename, thumb_size)
     else
-      image_thumbnail(filename, thumb_filename)
+      image_thumbnail(filename, thumb_filename, thumb_size)
     end or
-    icon_thumbnail(filename, thumb_filename)
+    icon_thumbnail(filename, thumb_filename, thumb_size)
   end
 
-  def icon_thumbnail(filename, thumb_filename)
-    icon.cp(thumb_filename)
+  def icon_thumbnail(filename, thumb_filename, thumb_size=128)
+    icon(thumb_size).cp(thumb_filename)
   end
 
-  def icon
+  def icon(thumb_size)
     ancestors.map do |klass|
-      Future.icon_dir + (klass.to_s.gsub(/\//, '_')+".png")
+      Future.icon_dir + thumb_size.to_s + (klass.to_s.gsub(/\//, '_')+".png")
     end.find{|pn| pn.exist? }
   end
 
-  def image_thumbnail(filename, thumb_filename)
-    system("convert", filename, "-scale", '128x128>', thumb_filename)
+  def image_thumbnail(filename, thumb_filename, thumb_size)
+    system("convert", filename, "-scale", "#{thumb_size}x#{thumb_size}>", thumb_filename)
     File.exist?(thumb_filename)
   end
 
-  def video_thumbnail(filename, thumb_filename)
+  def video_thumbnail(filename, thumb_filename, thumb_size)
     video_cache_dir = Future.cache_dir + 'videotemp'
     video_cache_dir.mkdir_p
     video_cache_dir.glob("*").each{|e| e.unlink }
     system("mplayer", "-nosound", "-ss", "5.7", "-vf", "scale",
            "-vo", "jpeg:outdir=#{video_cache_dir}", "-frames", "10", filename)
     j = Dir[video_cache_dir + "*.jpg"].sort.last
-    image_thumbnail(j, thumb_filename) if j
+    image_thumbnail(j, thumb_filename, thumb_size) if j
     File.exist?(thumb_filename)
   end
   
