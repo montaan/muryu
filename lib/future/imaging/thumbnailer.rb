39f25ceb567c3b8823df452d9345f910310ee1cd
kig <kig@misfiring.net>
2006-12-19 12:47:22 +0000

    advanced thumbnailer with support for rendering a wanted page of a pdf at the wanted size and crop and adjustable video frame capture start time

--- lib/future/imaging/thumbnailer.rb
+++ lib/future/imaging/thumbnailer.rb
@@ -17,8 +17,8 @@ end
 
 class Pathname
 
-  def thumbnail(thumb_filename, thumb_size=128)
-    mimetype.thumbnail(self, thumb_filename, thumb_size)
+  def thumbnail(thumb_filename, *args)
+    mimetype.thumbnail(self, thumb_filename, *args)
   end
 
   def mimetype
@@ -30,17 +30,19 @@ end
 
 module Mimetype
 
-  def thumbnail(filename, thumb_filename, thumb_size=128)
+  def thumbnail(filename, thumb_filename, thumb_size=128, page=nil, crop='0x0+0+0')
     if to_s =~ /video/
-      video_thumbnail(filename, thumb_filename, thumb_size)
+      page ||= 5.7
+      video_thumbnail(filename, thumb_filename, thumb_size, page, crop)
     else
-      image_thumbnail(filename, thumb_filename, thumb_size)
+      page ||= 0
+      image_thumbnail(filename, thumb_filename, thumb_size, page, crop)
     end or
-    icon_thumbnail(filename, thumb_filename, thumb_size)
+    icon_thumbnail(filename, thumb_filename, thumb_size, crop)
   end
 
-  def icon_thumbnail(filename, thumb_filename, thumb_size=128)
-    icon(thumb_size).cp(thumb_filename)
+  def icon_thumbnail(filename, thumb_filename, thumb_size=128, crop='0x0+0+0')
+    image_thumbnail(icon(thumb_size), thumb_filename, thumb_size, 0, crop)
   end
 
   def icon(thumb_size)
@@ -49,19 +51,35 @@ module Mimetype
     end.find{|pn| pn.exist? }
   end
 
-  def image_thumbnail(filename, thumb_filename, thumb_size)
-    system("convert", "#{filename}[0]", "-scale", "#{thumb_size}x#{thumb_size}", thumb_filename)
+  def image_thumbnail(filename, thumb_filename, thumb_size, page=0, crop='0x0+0+0')
+    dimstrs = `identify "#{filename}"[0]`.scan(/ [0-9]+x[0-9]+ /)
+    return false if dimstrs.empty?
+    dims = dimstrs[0].strip.split("x").map{|c|c.to_f}
+    if dims.min < thumb_size
+      scale_fac = thumb_size / dims.min
+    elsif dims.max > thumb_size
+      scale_fac = thumb_size / dims.max
+    else
+      scale_fac = 1
+    end
+    density = scale_fac * 72
+    args = ["-density", density.to_s,
+            "#{filename}[#{page}]",
+            "-scale", "#{thumb_size}x#{thumb_size}",
+            "-crop", crop.to_s,
+            thumb_filename]
+    system("convert", *args)
     File.exist?(thumb_filename)
   end
 
-  def video_thumbnail(filename, thumb_filename, thumb_size)
+  def video_thumbnail(filename, thumb_filename, thumb_size, page, crop)
     video_cache_dir = Future.cache_dir + 'videotemp'
     video_cache_dir.mkdir_p
     video_cache_dir.glob("*").each{|e| e.unlink }
-    system("mplayer", "-nosound", "-ss", "5.7", "-vf", "scale",
+    system("mplayer", "-nosound", "-ss", page.to_s, "-vf", "scale",
            "-vo", "jpeg:outdir=#{video_cache_dir}", "-frames", "10", filename)
     j = Dir[video_cache_dir + "*.jpg"].sort.last
-    image_thumbnail(j, thumb_filename, thumb_size) if j
+    image_thumbnail(j, thumb_filename, thumb_size, 0, crop) if j
     File.exist?(thumb_filename)
   end
   
