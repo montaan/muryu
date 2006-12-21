8548a0666c81115e78fa18fa5a48eae628942a67
kig <kig@misfiring.net>
2006-12-21 07:26:41 +0000

    full_text_search.sql: fixed

--- database/full_text_search.sql
+++ database/full_text_search.sql
@@ -1,8 +1,6 @@
-post_sql << <<-EOF
 ALTER TABLE itemtexts ADD COLUMN fti_vector tsvector;
 CREATE TRIGGER tsvectorupdate BEFORE UPDATE OR INSERT ON itemtexts
     FOR EACH ROW EXECUTE PROCEDURE tsearch2(fti_vector, text);
 
 CREATE INDEX itemtexts_fti_idx ON itemtexts USING gist(fti_vector);
 VACUUM FULL ANALYZE;
-EOF
