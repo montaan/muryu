eaf15fda48541a00771453e4f3a2d0e9b2b1f058
Mauricio Fernandez <mfp@acm.org>
2007-01-11 12:03:18 +0000

    imaging/distributed_tile_drawer.rb: operate with draw_ops, split them correctly, skip empty subtiles.

--- lib/future/imaging/distributed_tile_drawer.rb
+++ lib/future/imaging/distributed_tile_drawer.rb
@@ -3,22 +3,29 @@ require 'future/imaging/image_cache'
 require 'future/imaging/tile_drawer'
 require 'digest/sha1'
 require 'imlib2'
+require 'enumerator'
 
-class TileJobRequest < Struct.new(:jobid, :x, :y, :w, :h, :zoom, :indexes, :layouter_name, :layouter_args)
+class TileJobRequest < Struct.new(:jobid, :x, :y, :w, :h, :zoom, :draw_ops)
 end
 
-class TileJobResult < Struct.new(:jobid, :raw_data)
+class TileJobResult < Struct.new(:jobid, :x, :y, :raw_data)
 end
 
 class DTileClient
   TUPLE_EXPIRATION_TIME = 10 # seconds
-  def initialize(tuplespace)
+  def initialize(tuplespace, tile_drawer)
     @tuplespace = tuplespace
+    @tile_drawer = tile_drawer
   end
 
   def draw_tile(indexes, layouter_name, x, y, zoom, w, h, *layouter_args)
+    draw_ops = []
+    @tile_drawer.tile_info(indexes, layouter_name, x, y, zoom, w, h, *layouter_args) do |i, ix, iy, sz|
+      draw_ops.concat([ix, iy, i])
+    end
+
     jobid   = "#{Process.pid}#{Thread.current.object_id}-#{x}-#{y}-#{w}-#{h}"
-    tilejob = TileJobRequest.new(jobid, x, y, w, h, zoom, indexes, layouter_name, layouter_args)
+    tilejob = TileJobRequest.new(jobid, x, y, w, h, zoom, draw_ops)
     log_debug("Placing request #{jobid} (#{w}x#{h})")
     @tuplespace.write([:tilejob_request, jobid, tilejob], TUPLE_EXPIRATION_TIME)
     result = @tuplespace.read([:tilejob_result, jobid, nil])[2]
@@ -32,9 +39,10 @@ class DTileServer
   MAX_THUMBS_PER_JOB    = 16
   TUPLE_EXPIRATION_TIME = 10 # seconds
 
-  def initialize(tuplespace, tile_drawer, max_thumbs_per_job = MAX_THUMBS_PER_JOB, branching_factor = 4)
-    @tile_drawer        = tile_drawer
+  def initialize(tuplespace, image_cache = Future.image_cache, 
+                 max_thumbs_per_job = MAX_THUMBS_PER_JOB, branching_factor = 4)
     @tuplespace         = tuplespace
+    @image_cache        = image_cache
     @max_thumbs_per_job = max_thumbs_per_job
     @branching_factor   = branching_factor
   end
@@ -51,16 +59,22 @@ class DTileServer
   end
 
   def process_tilejob(tilejob)
-    if tilejob.w * tilejob.h / (2**tilejob.zoom) ** 2 > @max_thumbs_per_job
+    if tilejob.draw_ops.size/3 > @max_thumbs_per_job
       split_tilejob(tilejob)
     else
       sz = 2 ** tilejob.zoom
       log_debug("Drawing tile (#{tilejob.x},#{tilejob.y}) #{tilejob.w}x#{tilejob.h}")
-      tile = @tile_drawer.draw_tile(tilejob.indexes, tilejob.layouter_name,
-                                    tilejob.x, tilejob.y, tilejob.zoom, tilejob.w,
-                                    tilejob.h, *tilejob.layouter_args)
+
+      tile = Imlib2::Image.new(tilejob.w, tilejob.h)
+      tile.clear
+      @image_cache.batch do
+        tilejob.draw_ops.each_slice(3) do |ix, iy, index|
+          @image_cache.draw_image_at(index, tilejob.zoom, tile, ix, iy)
+        end
+      end
       # FIXME: rescue exception
-      tilejob_result = TileJobResult.new(tilejob.jobid, tile ? tile.data! : nil)
+      tilejob_result = TileJobResult.new(tilejob.jobid, tilejob.x, tilejob.y, 
+                                         tile ? tile.data! : nil)
       log_debug("Placing result for #{tilejob.jobid}")
       @tuplespace.write([:tilejob_result, tilejob.jobid, tilejob_result], 
                         TUPLE_EXPIRATION_TIME)
@@ -73,50 +87,54 @@ class DTileServer
     jobs = []
     thsz = 2 ** tilejob.zoom
 
-    log_debug("Splitting tilejob #{tilejob.jobid} (#{w}x#{h}) #{tilejob.indexes.size} indexes @ zoom #{tilejob.zoom}")
-    subtiles = []
-    @branching_factor.times do |i|
-      @branching_factor.times do |j|
-        subtiles << [i * w/@branching_factor/thsz, j * w/@branching_factor/thsz]
-      end
+    log_debug("Splitting tilejob #{tilejob.jobid} (#{w}x#{h}) #{tilejob.draw_ops.size/3} drawops @ zoom #{tilejob.zoom}")
+    subtile_matrix = (0...@branching_factor).map{ (0...@branching_factor).map{[]} }
+    subw = tilejob.w / @branching_factor
+    subh = tilejob.h / @branching_factor
+    tilejob.draw_ops.each_slice(3) do |ix, iy, index|
+      subtile_matrix[(ix - tilejob.x) / subw][(iy - tilejob.y) / subh] << [ix, iy, index]
     end
-    log_debug("Subtiles: #{subtiles.inspect}")
+    log_debug("Subtile matrix':\n" + 
+              subtile_matrix.map{|col| col.map{|x| "%4d" % x.size}.join(" ") }.join("\n"))
 
-    subtiles.each_with_index do |(x1, y1), idx|
-      indexes = (y1..y1+h/@branching_factor/thsz-1).inject([]) do |s,i|
-        s.concat(tilejob.indexes[i * w/thsz + x1, w/@branching_factor/thsz])
+    empty_img = Imlib2::Image.new(subw, subh)
+    empty_img.clear
+    empty_subtile_data = empty_img.data!
+
+    results = []
+
+    subtile_matrix.each_with_index do |column, i|
+      column.each_with_index do |subtile_ops, j|
+        draw_ops = subtile_ops.flatten
+        subtile_jobid = tilejob.jobid + "-#{i},#{j}"
+        if draw_ops.size > 0
+          jobs << TileJobRequest.new(subtile_jobid,
+                                     tilejob.x + i*subw, tilejob.y + j*subh, 
+                                     subw, subh, tilejob.zoom, draw_ops)
+        else
+          log_debug("Subtile #{i} #{j} is empty, skipping tilejob.")
+          results << TileJobResult.new(subtile_jobid, 
+                                       tilejob.x + i * subw, tilejob.y + j * subh, empty_subtile_data)
+        end
       end
-      jobs << TileJobRequest.new(tilejob.jobid + "-sub#{idx}", tilejob.x+x1*thsz, tilejob.y+y1*thsz, 
-                                 tilejob.w/@branching_factor, tilejob.h/@branching_factor, tilejob.zoom,
-                                 indexes, tilejob.layouter_name, tilejob.layouter_args)
     end
     log_debug("Placing tilejobs #{jobs.map{|x| x.jobid}.join(', ')}")
     jobs.each{|job| @tuplespace.write([:tilejob_request, job.jobid, job], TUPLE_EXPIRATION_TIME)}
     # FIXME: timeout & request
-    results = []
     jobs.each do |job|
       results << @tuplespace.read([:tilejob_result, job.jobid, nil])[2]
       log_debug("Received #{results.last.jobid}")
     end
     tile = Imlib2::Image.new(tilejob.w, tilejob.h)
     tile.clear
-    img = Imlib2::Image.new(tilejob.w / @branching_factor, tilejob.h / @branching_factor)
-    empty_data = nil
-    subtiles.each_with_index do |(x1, y1), i|
-      data = results[i].raw_data
-      unless data
-        unless empty_data
-          empty_img = Imlib2::Image.new(tilejob.w/@branching_factor, tilejob.h/@branching_factor)
-          empty_img.clear
-          empty_data = empty_img.data!
-        end
-        data = empty_data
-      end
+    img = Imlib2::Image.new(subw, subh)
+    results.each do |tilejob_result|
+      data = tilejob_result.raw_data || empty_subtile_data
       img.data = data
-      tile.blend!(img, 0, 0, w/@branching_factor, h/@branching_factor, x1, y1, 
-                  w/@branching_factor, h/@branching_factor)
+      tile.blend!(img, 0, 0, subw, subh, 
+                  tilejob_result.x - tilejob.x, tilejob_result.y - tilejob.y, subw, subh)
     end
-    tilejob_result = TileJobResult.new(tilejob.jobid, tile.data!)
+    tilejob_result = TileJobResult.new(tilejob.jobid, tilejob.x, tilejob.y, tile.data!)
     log_debug("Placing composed result for #{tilejob.jobid}")
     @tuplespace.write([:tilejob_result, tilejob.jobid, tilejob_result], TUPLE_EXPIRATION_TIME)
   end
@@ -136,7 +154,7 @@ if __FILE__ == $0
     end
     DRb.start_service
     tuplespace = DRbObject.new(nil, ARGV[1])
-    dtile_client = DTileClient.new(tuplespace)
+    dtile_client = DTileClient.new(tuplespace, Future::TileDrawer.new)
     tile = dtile_client.draw_tile((0..256**2 / 16**2).to_a, "rows", 0, 0, 4, 256, 256)
     p tile
   when "inspect"
@@ -154,7 +172,7 @@ if __FILE__ == $0
     end
     tuplespace = DRbObject.new(nil, ARGV[1])
     threads = (0..20).map do
-      dtile_server = DTileServer.new(tuplespace, Future::TileDrawer.new)
+      dtile_server = DTileServer.new(tuplespace)
       dtile_server.thread
     end
     puts "Looping"
