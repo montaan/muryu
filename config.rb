223ae433d3a30a713cc8f3e70534a968ca61aaad
kig <kig@misfiring.net>
2007-08-04 12:58:21 +0000

    more setup conveniences
    future_setup: 
      * take tile server subdomains and certificate filenames as arguments
      * generate certificates if missing
    
    nginx.conf.erb:
      * get username from ENV["USER"]
      * use cert filenames from future_setup
      * drop :port if port is default
      * get html root from ENV["PWD"]
    
    config.rb.erb: 
      * drop :port if port is default
      * use $tile_subdomains for setting tile_servers

--- config.rb
+++ config.rb
@@ -15,7 +15,19 @@ database_conf =
   },
 }
 
+
 Future.configuration do |c|
-  c.database_configuration = database_conf[c.environment]
+  # c.database_configuration = database_conf[c.environment]
   # c.logger = Logger.new(c.log_dir + "#{c.environment}.log")
+
+  c.domain = "manifold.fhtr.org"
+  c.server_name        =  "http://manifold.fhtr.org:8080"
+  c.secure_server_name = "https://manifold.fhtr.org:8081"
+  if $tile_subdomains
+    c.tile_servers = $tile_subdomains.map{|ts| 
+      c.server_name.sub(/^http:\/\//, "http://#{ts}.") + "/tile/" 
+    }
+  else
+    c.tile_servers = [ c.server_name + "/tile/" ]
+  end
 end
